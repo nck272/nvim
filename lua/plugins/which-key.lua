a529289951b62e2ab344218392904675676d9f27
nck272 <nck272.dev@gmail.com>
2025-12-29 22:50:03 +0700

    Add oil&harpoon group

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,15 +1,18 @@
 return {
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            preset = "classic",
-            icons = {
-                colors = true,
-            },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        preset = "classic",
+        icons = {
+            colors = true,
         },
-        init = function()
-            vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "NONE" })
-        end,
+        spec = {
+            { "<leader>o", group = "oil", icon = "󰉖" },
+            { "<leader>h", group = "harpoon", icon = "󰛢" },
+        },
     },
+    init = function()
+        vim.api.nvim_set_hl(0, "WhichKeyIcon", { underline = false, bold = false })
+        vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "NONE" })
+    end,
 }
