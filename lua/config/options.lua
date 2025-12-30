6f0ed4246a4c0b63a90845fff1c0ddef6afc4f04
nck272 <nck272.dev@gmail.com>
2025-12-30 23:47:37 +0700

    move set term truecolor to option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,9 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.expandtab = true
 
+-- Enable truecolor support
+opt.termguicolors = true
+
 -- This makes nvim use the system clipboard by default for all yanks
 vim.opt.clipboard = "unnamedplus"
 
