518243ec6bc25fad7078613cbd360a9e86324251
nck272 <nck272.dev@gmail.com>
2025-12-31 23:07:38 +0700

    fix options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,16 +30,16 @@ opt.wrap = true
 opt.linebreak = true
 
 if vim.fn.has("wsl") == 1 then
-    vim.g.clipboard = {
-        name = "WslClipboard",
-        copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe",
-        },
-        paste = {
-            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
 end
--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -1,29 +1,31 @@
 -- Set local settings for terminal buffers
 vim.api.nvim_create_autocmd("TermOpen", {
-    group = vim.api.nvim_create_augroup("custom-term-open", {}),
-    callback = function()
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
-        vim.opt_local.scrolloff = 0
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.scrolloff = 0
 
-        vim.bo.filetype = "terminal"
-    end,
+    vim.bo.filetype = "terminal"
+  end,
 })
+
+local set = vim.keymap.set
 
 -- Easily hit escape in terminal mode.
 set("t", "<esc><esc>", "<c-\\><c-n>")
 
 -- Open a terminal at the bottom of the screen with a fixed height.
 set("n", "<leader>tv", function()
-    vim.cmd.new()
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 12)
-    vim.wo.winfixheight = true
-    vim.cmd.term()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
 end)
 
 set("n", "<leader>th", function()
-    vim.cmd.vsplit()
-    vim.api.nvim_win_set_width(0, 80)
-    vim.cmd.term()
+  vim.cmd.vsplit()
+  vim.api.nvim_win_set_width(0, 80)
+  vim.cmd.term()
 end)
