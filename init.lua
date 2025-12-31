fa063accac7ceb96ba32fc09830dae9a15cf76a1
nck272 <nck272.dev@gmail.com>
2025-12-31 18:06:07 +0700

    disable word highlight

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -18,21 +18,22 @@ vim.opt.rtp:prepend(lazypath)
 local scheme = "gruvbuddy"
 
 require("lazy").setup({
-    spec = {
-        {
-            "LazyVim/LazyVim",
-            import = "lazyvim.plugins",
-            opts = {
-                colorscheme = scheme,
-            },
-        },
-        { import = "plugins" },
-        { "folke/noice.nvim", enabled = false },
+  spec = {
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = scheme,
+      },
     },
-    defaults = {
-        lazy = false,
-        version = false,
-    },
+    { import = "plugins" },
+    { "folke/noice.nvim", enabled = false },
+    { "snacks.nvim", opts = { words = { enabled = false } } },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
 })
 
 vim.cmd.colorscheme(scheme)
