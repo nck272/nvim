26f31d128335c6f94c48bf8846296715ca84fef1
nck272 <nck272.dev@gmail.com>
2025-12-31 23:23:15 +0700

    remove init

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,15 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        preset = "classic",
-        icons = {
-            colors = true,
-        },
-        spec = {
-            { "<leader>o", group = "oil", icon = "󰉖" },
-            { "<leader>h", group = "harpoon", icon = "󰛢" },
-        },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "classic",
+    icons = {
+      colors = true,
     },
-    init = function()
-        vim.api.nvim_set_hl(0, "WhichKeyIcon", { underline = false, bold = false })
-        vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "NONE" })
-    end,
+    spec = {
+      { "<leader>o", group = "oil", icon = "󰉖" },
+      { "<leader>h", group = "harpoon", icon = "󰛢" },
+      { "<leader>t", group = "terminal", icon = "󰞷" },
+    },
+  },
 }
