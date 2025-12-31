10f426c1039a0d762ee18062b840454215e383d2
nck272 <nck272.dev@gmail.com>
2025-12-31 11:57:11 +0700

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,15 +1,30 @@
 local opt = vim.opt
 
+-- Spacing
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.expandtab = true
 
+-- Search setting
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.signcolumn = "yes"
+
 -- Enable truecolor support
 opt.termguicolors = true
 
 -- This makes nvim use the system clipboard by default for all yanks
-vim.opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
+
+opt.winbar = "%f %h%w%m%r"
+
+opt.wrap = true
+opt.linebreak = true
 
 if vim.fn.has("wsl") == 1 then
     vim.g.clipboard = {
