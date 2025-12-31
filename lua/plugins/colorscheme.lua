1c344080462801f3e21843e5c7574fee897cdf2c
nck272 <nck272.dev@gmail.com>
2025-12-31 16:32:51 +0700

    use lualine instead of statusline

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,7 +10,14 @@ return {
     { "folke/tokyonight.nvim" },
     { "rose-pine/neovim" },
     { "Mofiqul/dracula.nvim" },
-    { "navarasu/onedark.nvim" },
+    {
+        "navarasu/onedark.nvim",
+        config = function()
+            require("onedark").setup({
+                style = "darker",
+            })
+        end,
+    },
     { "ribru17/bamboo.nvim" },
     { "catppuccin/nvim" },
     { "savq/melange-nvim" },
@@ -19,4 +26,11 @@ return {
     { "aliqyan-21/darkvoid.nvim" },
     { "ficcdaf/ashen.nvim" },
     { "https://gitlab.com/motaz-shokry/gruvbox.nvim" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = scheme,
+        },
+        -- Do not add config here, it will override all lazy plugins
+    },
 }
