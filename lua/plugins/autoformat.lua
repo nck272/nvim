91f215d06a3fac57e3be94467c4175112bc46f0f
nck <nck272.dev@gmail.com>
2026-01-08 23:31:12 +0700

    update auto format for lazy

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -13,18 +13,43 @@ return {
         desc = "Format Injected Langs",
       },
     },
+    init = function()
+      -- Install the conform formatter on VeryLazy
+      LazyVim.on_very_lazy(function()
+        LazyVim.format.register({
+          name = "conform.nvim",
+          priority = 100,
+          primary = true,
+          format = function(buf)
+            require("conform").format({ bufnr = buf })
+          end,
+          sources = function(buf)
+            local ret = require("conform").list_formatters(buf)
+            ---@param v conform.FormatterInfo
+            return vim.tbl_map(function(v)
+              return v.name
+            end, ret)
+          end,
+        })
+      end)
+    end,
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
         go = { "goimports", "gofmt" },
         ocaml = { "ocamlformat" }
       },
-      -- format_on_save = {
-      --   timeout_ms = 500,
-      --   lsp_format = "fallback",
-      -- },
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,           -- not recommended to change
+        quiet = false,           -- not recommended to change
+        lsp_format = "fallback", -- not recommended to change
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      }
     },
   }
 }
