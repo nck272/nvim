edd85878ae2f6d6b6c94380fac7354a9b85c4abf
nck272 <nck272.dev@gmail.com>
2026-01-26 23:58:06 +0700

    Update JDTLS setup for user-specific paths

--- lua/jdtls/jdtls_setup.lua
+++ lua/jdtls/jdtls_setup.lua
@@ -1,12 +1,10 @@
 local M = {}
-
 function M:setup()
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-  local workspace_dir = "/opt/jdtls/development/jdtls_java/" .. project_name
+  local workspace_dir = vim.fn.expand("~/.jdtls/development/jdtls_java/") .. project_name
   local config = {
     cmd = {
       "/usr/lib/jvm/java-21-openjdk-amd64/bin/java",
-
       "-Declipse.application=org.eclipse.jdt.ls.core.id1",
       "-Dosgi.bundles.defaultStartLevel=4",
       "-Declipse.product=org.eclipse.jdt.ls.core.product",
@@ -18,25 +16,31 @@ function M:setup()
       "java.base/java.util=ALL-UNNAMED",
       "--add-opens",
       "java.base/java.lang=ALL-UNNAMED",
-
       "-jar",
-      "/opt/jdtls/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
-
+      vim.fn.expand("~/.jdtls/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar"),
       "-configuration",
-      "/opt/jdtls/config_linux",
-
+      vim.fn.expand("~/.jdtls/config_linux"),
       "-data",
       workspace_dir,
     },
     root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
     settings = {
-      java = {},
+      java = {
+        configuration = {
+          runtimes = {
+            {
+              name = "JavaSE-21",
+              path = "/usr/lib/jvm/java-21-openjdk-amd64",
+              default = true,
+            },
+          },
+        },
+      },
     },
     init_options = {
       bundles = {},
     },
   }
-
   require("jdtls").start_or_attach(config)
 end
 
