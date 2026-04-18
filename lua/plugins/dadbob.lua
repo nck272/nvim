debbad613c6495b6c022c943877efdf6e2d99793
nck272 <nck272.dev@gmail.com>
2026-04-18 19:14:48 +0700

    udpate dadbob

--- lua/plugins/dadbob.lua
+++ lua/plugins/dadbob.lua
@@ -1,5 +1,16 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
   "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod" },
+    {
+      "kristijanhusak/vim-dadbod-completion",
+      ft = { "sql", "mysql", "plsql" },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "vim-dadbod-completion" })
+    end,
+  },
 }
